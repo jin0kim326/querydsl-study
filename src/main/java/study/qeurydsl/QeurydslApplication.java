0e6fe22be904671267f49b0b32c8c182726730a9
jin0kim326 <jin0kim326@douzone.com>
2022-04-23 16:33:44 +0900

    init querydsl

---
+++ src/main/java/study/qeurydsl/QeurydslApplication.java
@@ -0,0 +1,13 @@
+package study.qeurydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QeurydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QeurydslApplication.class, args);
+	}
+
+}

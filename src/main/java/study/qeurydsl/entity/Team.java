45b1e900a599cc501d3bccb932de2a3817567595
jin0kim326 <jin0kim326@douzone.com>
2022-04-23 17:30:08 +0900

    jpql vs querydsl

---
+++ src/main/java/study/qeurydsl/entity/Team.java
@@ -0,0 +1,30 @@
+package study.qeurydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
--- src/test/java/study/qeurydsl/QeurydslApplicationTests.java
+++ src/test/java/study/qeurydsl/QeurydslApplicationTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.qeurydsl.entity.Hello;
 import study.qeurydsl.entity.QHello;
@@ -14,6 +15,7 @@ import javax.persistence.EntityManager;
 import static org.assertj.core.api.Assertions.*;
 
 @Transactional
+@Commit
 @SpringBootTest
 class QeurydslApplicationTests {
 
